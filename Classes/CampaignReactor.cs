60f91d0acb0dc428f944e980d3d1735603c00f4e
Marc Martinez <universal11@gmail.com>
2016-01-28 16:39:44 -0800

    migrated majority of workload to server due to dependency constraints

--- Classes/CampaignReactor.cs
+++ Classes/CampaignReactor.cs
@@ -1,38 +1,17 @@
-﻿using BitlyDotNET.Exceptions;
-using BitlyDotNET.Implementations;
-using BitlyDotNET.Interfaces;
-using libCampaignReactor.Models;
-using MailKit;
-using MailKit.Net.Imap;
-using MailKit.Net.Smtp;
-using MailKit.Search;
-using MimeKit;
+﻿using libCampaignReactor.Models;
 using Newtonsoft.Json;
-using Renci.SshNet;
 using RestSharp;
 using RestSharp.Portable;
 using RestSharp.Portable.HttpClient;
-using S22.Imap;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Net;
-using System.Net.Mail;
-using System.Net.Sockets;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Windows.Storage;
+
 
 namespace CampaignReactorClient.Classes {
     public class CampaignReactor {
         //private string host = "localhost";
         //private int port = 3579;
 
-        private const string CMD_GET_IPV4_ADDRESSES = "ifconfig | awk -F \"[: ]+\" '/inet addr:/ { if ($4 != \"127.0.0.1\") print $4 }'";
-        private const string CMD_GET_IPV6_ADDRESSES = "ifconfig | awk -F \"[: ]+\" '/inet6 addr:/ { if ($4 != \"127.0.0.1\") print $4 }'";
-        private const string YAHOO_IMAP_HOST = "imap.mail.yahoo.com";
 
         public CampaignReactor() {
 
@@ -580,71 +559,6 @@ namespace CampaignReactorClient.Classes {
             return JsonConvert.DeserializeObject<List<EventLog>>(CampaignReactor.getResponse(response.RawBytes));
         }
 
-        public List<Host> getHosts(Server server) {
-            List<Host> hosts = new List<Host>();
-            SshClient sshClient = new SshClient(server.address, server.username, server.password);
-            sshClient.Connect();
-
-            SshCommand command = sshClient.RunCommand(CampaignReactor.CMD_GET_IPV4_ADDRESSES);
-
-            string[] addresses = command.Result.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (string address in addresses) {
-                Host host = new Host();
-                host.serverId = server.id;
-                host.address = address;
-
-                host = this.getHostById(this.createHost(host));
-                hosts.Add(host);
-            }
-
-            return hosts;
-        }
-
-        public string shortenUrl(BitlyAccount bitlyAccount, string url) {
-            
-            try{
-                IBitlyService bitlyService = new BitlyService(bitlyAccount.username, bitlyAccount.apiKey);
-                url = bitlyService.Shorten(url);
-            }
-            catch (BitlyDotNETException exception) {
-                System.Diagnostics.Debug.WriteLine($"Bitly Exception: {exception.Reason.ToString()}");
-            }
-            return url;
-        }
-
-        private async void sendEmail(libCampaignReactor.Models.Campaign campaign, libCampaignReactor.Models.Bot bot, libCampaignReactor.Models.BitlyAccount bitlyAccount, libCampaignReactor.Models.Host host, libCampaignReactor.Models.Subscriber subscriber) {
-            MimeMessage message = new MimeMessage();
-            message.From.Add(new MailboxAddress($"{bot.firstName} {bot.lastName}", bot.emailAddress));
-            message.To.Add(new MailboxAddress(subscriber.fullName, subscriber.emailAddress));
-            message.Subject = "How you doin'?";
-
-            int sendTime = (int)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
-            string campaignUrl = this.shortenUrl(bitlyAccount, $"http://campaignreactor.com:3579/event_log/click/?cid={campaign.id}&bid={bot.id}&hid={host.id}&svid={host.serverId}&sid={subscriber.id}&uid=1&st={sendTime}");
-            string campaignImageUrl = this.shortenUrl(bitlyAccount, $"http://campaignreactor.com:3579/event_log/open/?cid={campaign.id}&bid={bot.id}&hid={host.id}&svid={host.serverId}&sid={subscriber.id}&uid=1&st={sendTime}");
-            string campaignUnsubscribeUrl = this.shortenUrl(bitlyAccount, $"http://campaignreactor.com:3579/event_log/unsubscribe/?cid={campaign.id}&bid={bot.id}&hid={host.id}&svid={host.serverId}&sid={subscriber.id}&uid=1&st={sendTime}");
-            string campaignUnsubscribeImageUrl = this.shortenUrl(bitlyAccount, $"http://campaignreactor.com:3579/campaign/unsubscribe_image/?cid={campaign.id}&bid={bot.id}&hid={host.id}&svid={host.serverId}&sid={subscriber.id}&uid=1&st={sendTime}");
-
-            message.Body = new BodyBuilder() {
-                TextBody = campaign.subject,
-                HtmlBody = $"<center><a href=\"{campaignUrl}\">{campaign.subject}</a><br/><br/><a href=\"{campaignUrl}\"><img style=\"width=\"300px\" height=\"200px\" src=\"{campaignImageUrl}\" /></a><br/><br/><a href=\"{campaignUnsubscribeUrl}\"><img style=\"width=\"300px\" height=\"200px\" src=\"{campaignUnsubscribeImageUrl}\" /></a></center>"
-            }.ToMessageBody();
-            SmtpClient client = new SmtpClient();
-            client.Connect("smtp.mail.yahoo.com", 587, false);
-
-            // Note: since we don't have an OAuth2 token, disable
-            // the XOAUTH2 authentication mechanism.
-            client.AuthenticationMechanisms.Remove("XOAUTH2");
-
-            // Note: only needed if the SMTP server requires authentication
-            client.Authenticate("joey", "password");
-
-            client.Send(message);
-            client.Disconnect(true);
-
-        }
-
-
         public int markBotAsSent(Bot bot) {
             RestClient client = new RestClient(this.getAPIUri());
 
@@ -656,80 +570,6 @@ namespace CampaignReactorClient.Classes {
             // execute the request
             IRestResponse response = client.Execute(request).Result;
             return JsonConvert.DeserializeObject<int>(CampaignReactor.getResponse(response.RawBytes));
-        }
-
-        public void harvest(Bot bot) {
-            try {
-                ImapClient client = new ImapClient();
-                client.Connect(YAHOO_IMAP_HOST, 993, true);
-
-                // Note: since we don't have an OAuth2 token, disable
-                // the XOAUTH2 authentication mechanism.
-                client.AuthenticationMechanisms.Remove("XOAUTH2");
-
-                client.Authenticate(bot.emailAddress, bot.password);
-
-                // The Inbox folder is always available on all IMAP servers...
-                IMailFolder inbox = client.Inbox;
-                inbox.Open(FolderAccess.ReadWrite);
-
-                System.Diagnostics.Debug.WriteLine("Total messages: {0}", inbox.Count);
-                System.Diagnostics.Debug.WriteLine("Recent messages: {0}", inbox.Recent);
-
-                IList<UniqueId> uids = inbox.Search(SearchQuery.NotDeleted);
-
-                foreach (UniqueId uid in uids) {
-                    MimeMessage message = inbox.GetMessage(uid);
-                    Subscriber subscriber = new Subscriber();
-                    //InternetAddressList fromAddresses = message.From.ToArray()[0].Name;
-                    //string[] nameParts = message.From.ToArray()[0].Name.Split(null);
-                    subscriber.phoneNumber = null;
-                    //subscriber.firstName = nameParts[0];
-                    //subscriber.lastName = ((nameParts.Length > 1) ? nameParts[1] : "");
-                    subscriber.emailAddress = message.From.ToArray()[0].Name;
-
-                    subscriber.botId = bot.id;
-                    this.createSubscriber(subscriber);
-
-                    inbox.AddFlags(uid, MessageFlags.Deleted, true, System.Threading.CancellationToken.None);
-                    //var email = new Email(date: message.Date, id: message.MessageId, messageBody: message.TextBody, references: message.References);
-
-                }
-
-                
-                inbox.Expunge();
-                
-                client.Disconnect(true);
-
-                /*
-                ImapClient client = new ImapClient(YAHOO_IMAP_HOST, 993, bot.emailAddress, bot.password, AuthMethod.Login, true);
-                // Returns a collection of identifiers of all mails matching the specified search criteria.
-
-                IEnumerable <uint> uids = client.Search(SearchCondition.Undeleted());
-                // Download mail messages from the default mailbox.
-                
-
-                foreach (uint uid in uids) {
-                    MailMessage message = client.GetMessage(uid);
-                    Subscriber subscriber = new Subscriber();
-                    string[] nameParts = message.From.DisplayName.Split(null);
-                    subscriber.phoneNumber = null;
-                    subscriber.firstName = nameParts[0];
-                    subscriber.lastName = ((nameParts.Length > 1) ? nameParts[1] : "");
-                    subscriber.emailAddress = message.From.Address;
-                   
-                    subscriber.botId = bot.id;
-                    this.createSubscriber(subscriber);
-                    client.DeleteMessage(uid);
-                }
-                client.Dispose();
-                */
-            }
-
-            catch (Exception exception) {
-                System.Diagnostics.Debug.WriteLine($"Error: {exception.Message}");
-            }
-            
         }
 
 
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using libCampaignReactor.Models;
+using CampaignReactorClient.Classes;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -23,7 +24,7 @@ namespace CampaignReactorClient{
     /// </summary>
     public sealed partial class MainPage : Page{
 
-        public CampaignReactorClient client = new CampaignReactorClient();
+        public CampaignReactor client = new CampaignReactor();
 
         public MainPage(){
             this.InitializeComponent();
